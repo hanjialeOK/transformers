ffe35d7dcae82923376d834e56fa63389ee3ff67
韩佳乐 <hanjiale.123@bytedance.com>
2023-08-01 11:46:47 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,175 @@
+import tensorflow_datasets as tfds
+import tensorflow as tf
+
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+from transformer import Transformer, CustomSchedule
+from utils import create_masks
+
+if __name__ == '__main__':
+    # ==========================================================输入流水线==========================================================
+    examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en', with_info=True,
+                                as_supervised=True)
+    train_examples, val_examples = examples['train'], examples['validation']
+
+    # 从训练数据集创建自定义子词分词器（subwords tokenizer）。
+    tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+        (en.numpy() for pt, en in train_examples), target_vocab_size=2**13)
+
+    tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+        (pt.numpy() for pt, en in train_examples), target_vocab_size=2**13)
+
+    sample_string = 'Transformer is awesome.'
+
+    tokenized_string = tokenizer_en.encode(sample_string)
+    print ('Tokenized string is {}'.format(tokenized_string))
+
+    original_string = tokenizer_en.decode(tokenized_string)
+    print ('The original string: {}'.format(original_string))
+
+    assert original_string == sample_string
+
+    for ts in tokenized_string:
+        print ('{} ----> {}'.format(ts, tokenizer_en.decode([ts])))
+
+    # 将开始和结束标记（token）添加到输入和目标。
+    def encode(lang1, lang2):
+        lang1 = [tokenizer_pt.vocab_size] + tokenizer_pt.encode(
+            lang1.numpy()) + [tokenizer_pt.vocab_size+1]
+
+        lang2 = [tokenizer_en.vocab_size] + tokenizer_en.encode(
+            lang2.numpy()) + [tokenizer_en.vocab_size+1]
+
+        return lang1, lang2
+
+    def tf_encode(pt, en):
+        result_pt, result_en = tf.py_function(encode, [pt, en], [tf.int64, tf.int64])
+        result_pt.set_shape([None])
+        result_en.set_shape([None])
+
+        return result_pt, result_en
+
+    train_dataset = train_examples.map(tf_encode)
+    # 将数据集缓存到内存中以加快读取速度。
+    BUFFER_SIZE = 20000
+    BATCH_SIZE = 64
+    train_dataset = train_dataset.cache()
+    train_dataset = train_dataset.shuffle(BUFFER_SIZE).padded_batch(BATCH_SIZE)
+    train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+    val_dataset = val_examples.map(tf_encode)
+    val_dataset = val_dataset.padded_batch(BATCH_SIZE)
+
+    # 打印pt_batch, en_batch
+    pt_batch, en_batch = next(iter(val_dataset))
+    print(pt_batch)
+    print(en_batch)
+
+    # ==========================================================构建transformer与损失函数==========================================================
+    num_layers = 6
+    d_model = 512
+    dff = 2048
+    num_heads = 8
+
+    input_vocab_size = tokenizer_pt.vocab_size + 2
+    target_vocab_size = tokenizer_en.vocab_size + 2
+    dropout_rate = 0.1
+
+    transformer = Transformer(num_layers, d_model, num_heads, dff,
+                            input_vocab_size, target_vocab_size,
+                            pe_input=input_vocab_size,
+                            pe_target=target_vocab_size,
+                            rate=dropout_rate)
+
+    learning_rate = CustomSchedule(d_model)
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
+                                        epsilon=1e-9)
+
+    # 由于目标序列是填充（padded）过的，因此在计算损失函数时，应用填充遮挡非常重要。
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
+
+    def loss_function(real, pred):
+        mask = tf.math.logical_not(tf.math.equal(real, 0))
+        loss_ = loss_object(real, pred)
+
+        mask = tf.cast(mask, dtype=loss_.dtype)
+        loss_ *= mask
+
+        return tf.reduce_mean(loss_)
+
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+    # ==========================================================设置检查点并启动训练==========================================================
+    checkpoint_path = "./checkpoints/train"
+
+    ckpt = tf.train.Checkpoint(transformer=transformer, optimizer=optimizer)
+
+    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+    # 如果检查点存在，则恢复最新的检查点。
+    if ckpt_manager.latest_checkpoint:
+        ckpt.restore(ckpt_manager.latest_checkpoint)
+        print ('Latest checkpoint restored!!')
+
+
+    # 该 @tf.function 将追踪-编译 train_step 到 TF 图中，以便更快地
+    # 执行。该函数专用于参数张量的精确形状。为了避免由于可变序列长度或可变
+    # 批次大小（最后一批次较小）导致的再追踪，使用 input_signature 指定
+    # 更多的通用形状。
+
+    train_step_signature = [
+        tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+        tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+    ]
+
+    @tf.function(input_signature=train_step_signature)
+    def train_step(inp, tar):
+        tar_inp = tar[:, :-1]
+        tar_real = tar[:, 1:]
+
+        enc_padding_mask, combined_mask, dec_padding_mask = create_masks(inp, tar_inp)
+
+        with tf.GradientTape() as tape:
+            predictions, _ = transformer(inp, tar_inp,
+                                        True,
+                                        enc_padding_mask,
+                                        combined_mask,
+                                        dec_padding_mask)
+            loss = loss_function(tar_real, predictions)
+
+        gradients = tape.gradient(loss, transformer.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+
+        train_loss(loss)
+        train_accuracy(tar_real, predictions)
+
+    EPOCHS = 20
+
+    for epoch in range(EPOCHS):
+        start = time.time()
+
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+
+        # inp -> portuguese, tar -> english
+        for (batch, (inp, tar)) in enumerate(train_dataset):
+            train_step(inp, tar)
+
+            if batch % 50 == 0:
+                print ('Epoch {} Batch {} Loss {:.4f} Accuracy {:.4f}'.format(
+                    epoch + 1, batch, train_loss.result(), train_accuracy.result()))
+
+        if (epoch + 1) % 5 == 0:
+            ckpt_save_path = ckpt_manager.save()
+            print ('Saving checkpoint for epoch {} at {}'.format(epoch+1,
+                                                                ckpt_save_path))
+
+        print ('Epoch {} Loss {:.4f} Accuracy {:.4f}'.format(epoch + 1,
+                                                        train_loss.result(),
+                                                        train_accuracy.result()))
+
+        print ('Time taken for 1 epoch: {} secs\n'.format(time.time() - start))
